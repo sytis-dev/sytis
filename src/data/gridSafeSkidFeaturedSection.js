fd635fdf6f6ebcc5c2760638998387cfed25b237
ladellerby <ladellerby@gmail.com>
2025-04-16 15:20:58 -0700

    feat(page): add new grid safe system marketing page

---
+++ src/data/gridSafeSkidFeaturedSection.js
@@ -0,0 +1,151 @@
+import image1 from "@/images/resource/sytis-grid-safe-1.jpg";
+import image2 from "@/images/resource/sytis-grid-safe-2.jpg";
+import image3 from "@/images/resource/sytis-grid-safe-3.jpg";
+import image4 from "@/images/resource/sytis-grid-safe-4.jpg";
+import image5 from "@/images/resource/sytis-grid-safe-5.jpg";
+
+const gridSafeSkidFeaturedSection = [
+  {
+    image: image1,
+    title: "A Revolution in Utility Grid Protection and Monitoring Worldwide",
+    text: "Utilities often use traditional hand-held infrared camera technology, which is labor-intensive and provides only limited, momentary data that requires human manipulation and interpretation. Fixed-in-place optical security cameras require constant human monitoring. Otherwise, they simply collect a historic record of events to be viewed post-incident. <br/><br/> We have a better idea. Sytis has developed a game-changing product called GridSafe™. GridSafe™ combines bi-spectral (infrared/optical) PTZ cameras with our user-friendly InfraSpec™ software. This patent-pending software uses machine learning to track and notify utility personnel in real-time with both warnings and alerts related to physical threats (fire, eco-terrorism, trespassing, etc.) and/or component issues. And we do it while staying out of your network!​ ",
+    features: [],
+  },
+  {
+    image: image2,
+    title: "Comprehensive Substation Monitoring",
+    text: "Utility asset issues can escalate quickly. Traditional inspection programs often fail to provide actionable data or only collect information after problems arise. Scheduled inspections can mean missing opportunities for predictive maintenance, as issues usually arise under their own schedule. GridSafe™ enables users to monitor thousands of components with one camera and monitor beyond the fence-line at the same time using 'presets'. Seeing issues in their earliest stages allows for planning and ensures timely responses while preventing catastrophic failures or intrusions. <br/><br/> The system is built on a heavy galvanized steel skid, which is classified as a fixed asset but is still portable within substations for easy repositioning. It features a telescoping mast for high vantage points, and its network cabinet houses the NVR (Network Video Recorder), cellular router, backup power, and access to InfraSpec™ Software for seamless web-based monitoring.!​ ",
+    features: [],
+  },
+  {
+    image: image3,
+    title: "Key Features",
+    text: "​",
+    features: [
+      {
+        id: 1,
+        title: "24/7 Monitoring and Alerts",
+        text: "Proactively detect and respond to potential failures with historical data and real-time alerts.",
+        href: "",
+      },
+      {
+        id: 2,
+        title: "Radiometric Thermal Imaging",
+        text: "Detects temperature anomalies, providing early warnings for equipment failures.",
+        href: "",
+      },
+      {
+        id: 3,
+        title: "Fire Detection",
+        text: "The advanced infrared cameras and software allow the early detection of fires before they become an emergency. The system also gives the ability to visualize the fire even through thick smoke, improving response effectiveness.",
+        href: "",
+      },
+      {
+        id: 4,
+        title: "Up to 300 Preset Points of Interest",
+        text: "Monitor thousands of components from a single camera and receive instant email notifications for alarms and alerts. Pre-sets mean visual interpretation error is removed. If you get a notification, there is an actionable event.",
+        href: "",
+      },
+      {
+        id: 5,
+        title: "NDAA Compliant",
+        text: "The GridSafe™ System meets all required National Defense Authorization Act (NDAA) standards for security and safety.",
+        href: "",
+      },
+      {
+        id: 6,
+        title: "Network Security",
+        text: "We can stay out of your computer network and/or out of the cloud.",
+        href: "",
+      },
+      {
+        id: 7,
+        title: "Intelligent Bi-spectral Monitoring",
+        text: "Machine learning allows the system to provide intrusion detection, line crossing, smoke detection, automatic tracking of intruders, and human and vehicle recognition, ensuring comprehensive security.",
+        href: "",
+      },
+      {
+        id: 8,
+        title: "PI System integration",
+        text: "Can be integrated into the PI System with real-time video.",
+        href: "",
+      },
+    ],
+  },
+  {
+    image: image4,
+    title: "InfraSpec Software",
+    text: "InfraSpec™ is our proprietary web-based control center for the GridSafe™ System, offering the following features:",
+    features: [
+      {
+        id: 9,
+        title: "Access via Web Browser",
+        text: "When connected through a secure VPN or a utility's network, users can access the system directly via a web browser. This eliminates the need for local software installations.",
+        href: "",
+      },
+      {
+        id: 10,
+        title: "Customizable Dashboard",
+        text: "Add widgets and displays to tailor your experience.",
+        href: "",
+      },
+      {
+        id: 11,
+        title: "Real-Time Monitoring and Alerts",
+        text: "View live camera feeds and receive immediate notifications of triggered events.",
+        href: "",
+      },
+      {
+        id: 12,
+        title: "Playback and Snapshots",
+        text: "Review historical time-stamped footage and snapshots to investigate incidents.",
+        href: "",
+      },
+      {
+        id: 13,
+        title: "Time-Temperature Graphs",
+        text: "Access historical data for predictive maintenance with easy-to-read visualizations.",
+        href: "",
+      },
+      {
+        id: 14,
+        title: "PTZ Camera Control",
+        text: "Remotely control Pan-Tilt-Zoom cameras to focus on specific areas.",
+        href: "",
+      },
+    ],
+  },
+  {
+    image: image5,
+    title: "Why Choose the GridSafe™ System?",
+    text: "",
+    features: [
+      {
+        id: 15,
+        title: "Proactive Monitoring",
+        text: "Detect potential issues before they cause failures, ensuring operational efficiency and minimizing downtime.",
+        href: "",
+      },
+      {
+        id: 16,
+        title: "Reduce Truck Rolls",
+        text: "Minimize unnecessary site visits with real-time alerts and actionable data, sending the right team at the right time to address issues.",
+        href: "",
+      },
+      {
+        id: 17,
+        title: "Proactive Grid Operations",
+        text: "Predict and prevent catastrophic failure rather than responding to events after they happen.",
+        href: "",
+      },
+      {
+        id: 18,
+        title: "Money Savings",
+        text: "No need to install poles, power supplies, complex approvals, or environmental impact studies, making setup fast and cost-effective.",
+        href: "",
+      },
+    ],
+  },
+];
+
+export default gridSafeSkidFeaturedSection;
