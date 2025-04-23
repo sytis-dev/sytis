d1573a9b2480edf072dc32f9842e2c34e8712ddb
ladellerby <ladellerby@gmail.com>
2025-04-23 16:17:42 -0700

    fix: change const to let for products variable to allow reassignment

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
     }
 
     const productsData = await productsResponse.json();
-    const products = productsData.data || [];
+    let products = productsData.data || [];
     products = products.filter(
       (prod) => prod.is_visible
     );
