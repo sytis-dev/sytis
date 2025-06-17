3307b07486fc6898d68b988ed992f47115ac5635
ladellerby <ladellerby@gmail.com>
2025-06-17 13:57:45 -0700

    add Google Tag Manager integration to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,16 +10,39 @@ import "node_modules/swiper/swiper-bundle.min.css";
 import "react-circular-progressbar/dist/styles.css";
 import "react-modal-video/css/modal-video.min.css";
 import "tiny-slider/dist/tiny-slider.css";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 // extra css
 import "@/styles/style.css";
 import "@/styles/hover.css";
 import "@/styles/responsive.css";
 
+const GTM_ID = "GTM-WL832HQN";
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ContextProvider>
+      {/* ✅ GTM Script */}
+      <Script
+        id="gtm-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');`,
+        }}
+      />
+
+      {/* ✅ GTM noscript fallback */}
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      />
       <Component {...pageProps} />
       <Analytics />
     </ContextProvider>
