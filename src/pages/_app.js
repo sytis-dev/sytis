0ee977890540dd8685de72de886c7839dded24c4
ladellerby <ladellerby@gmail.com>
2025-06-19 15:57:07 -0700

    feat(CookieManager): integrate react-cookie-manager for enhanced cookie management

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,6 +12,8 @@ import "react-modal-video/css/modal-video.min.css";
 import "tiny-slider/dist/tiny-slider.css";
 import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
+import { CookieManager } from "react-cookie-manager";
+import "react-cookie-manager/style.css";
 
 // extra css
 import "@/styles/style.css";
@@ -22,30 +24,33 @@ const GTM_ID = "GTM-WL832HQN";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <ContextProvider>
-      {/* ✅ GTM Script */}
-      <Script
-        id="gtm-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <CookieManager cookieKitId="6854954ce5b3247b9f0e5e18">
+      {" "}
+      <ContextProvider>
+        {/* ✅ GTM Script */}
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${GTM_ID}');`,
-        }}
-      />
+          }}
+        />
 
-      {/* ✅ GTM noscript fallback */}
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+        {/* ✅ GTM noscript fallback */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-        }}
-      />
-      <Component {...pageProps} />
-      <Analytics />
-    </ContextProvider>
+          }}
+        />
+        <Component {...pageProps} />
+        <Analytics />
+      </ContextProvider>
+    </CookieManager>
   );
 };
 
